3d8a435049490dbd9b3654d2aa74d669c11a4254
DESKTOP-HGE6DGN\javi <csi22-jmennza@colegioaltair.net>
2022-11-21 13:28:37 +0100

    acabado estilos del detalles y logica

--- src/app/components/detalle-contactos/detalle-contactos.component.ts
+++ src/app/components/detalle-contactos/detalle-contactos.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { contactos } from "../../interfaces/contacto";
+import { ContactosService } from "../../services/contactos.service";
 
 @Component({
-  selector: 'app-detalle-contactos',
-  templateUrl: './detalle-contactos.component.html',
-  styleUrls: ['./detalle-contactos.component.css']
+  selector: "app-detalle-contactos",
+  templateUrl: "./detalle-contactos.component.html",
+  styleUrls: ["./detalle-contactos.component.css"],
 })
 export class DetalleContactosComponent implements OnInit {
+  constructor(
+    private cService: ContactosService,
+    private route: ActivatedRoute,
+  ) {}
 
-  constructor() { }
+  listaContactos?: contactos;
+
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      let id = Number(params.get("id"));
+      this.cService.getDetail(id).subscribe((resp: any) => {
+        this.listaContactos = resp.data;
+      });
+    });
   }
-
 }
--- src/app/services/contactos.service.ts
+++ src/app/services/contactos.service.ts
@@ -9,6 +9,10 @@ export class ContactosService {
   constructor(private http: HttpClient) {}
 
   getContactos() {
-    return this.http.get<any>('https://reqres.in/api/users?page=2');
+    return this.http.get<any>("https://reqres.in/api/users?page=2");
+  }
+
+  getDetail(id: number) {
+    return this.http.get<any>(`https://reqres.in/api/users/${id}`);
   }
 }
